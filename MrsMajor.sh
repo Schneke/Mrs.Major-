da147b0153e8fa83fc29801d703817f62401dbde
Schneke <40596381+Schneke@users.noreply.github.com>
2018-10-11 23:15:22 +0700

    Update and rename LITESPAM.sh to MrsMajor.sh

--- MrsMajor.sh
+++ MrsMajor.sh
@@ -1,14 +1,14 @@
 #!/system/xbin/bash
 clear
-toilet -f slant --gay "LiteSpam"
+toilet -f slant --gay "Mrs.Major"
 sleep 1
 echo "\033[31;1m Macam-macam tool untuk spam"
 sleep 1
-echo "\033[33;1m Author: Mr.Rm"
+echo "\033[33;1m Author: Mrs.Major"
 sleep 1
-echo "\033[32;1m ★contack: +15877711462"
+echo "\033[32;1m ★contack: 08557648078"
 sleep 1
-echo "\033[34;1m ★Indonesia Security Lite"
+echo "\033[34;1m ★Ciamis Black Hat's"
 sleep 1
 echo "\033[36;1m Spam Yang Tersedia :"
 echo "1. Tokopedia"
@@ -77,28 +77,28 @@ if
 [ $mrrm = 81 ] || [ $mrrm = 81 ]
 then
 clear
-toilet -f slant --gay "LiteSpam"
-echo "\033[31;1mNama tools: LiteSpam"
+toilet -f slant --gay "Mrs.Major"
+echo "\033[31;1mNama tools: Mrs.Major"
 sleep 1
-echo "\033[33;1mKarya: Mr.Rm (V2 By Mr.IM81)"
+echo "\033[33;1mKarya: Mrs.Major (V2 By Mrs.Major)"
 sleep 1
 echo "\033[32;1mVersi: v2"
 sleep 1
-echo "\033[36;1mTeam: ★Indonesia Security Lite★"
+echo "\033[36;1mTeam: ★Ciamis Black Hat's★"
 sleep 1
-echo "\033[34;1mInformasi Lebih Lanjut Kunjungi: https://youtube.com/IndonesiaSecurityLite"
+echo "\033[34;1mInformasi Lebih Lanjut Kunjungi: https://ciamis-technologi.blogspot.com"
 echo "\033[31;1m Spesial Thanks To: "
 echo "\033[36;1m"
-echo "Handika Pratama"
+echo "Mrs.Major"
 echo "まやちゃん"
-echo "Baby Cyber Team"
-echo "SBG-Team"
-echo "Ponorogo Defacer Team"
+echo "Ciamis Black Hat's"
+echo "CBH-Team"
+echo "Ciamis Defacer Team"
 sleep 1
-echo "And All Member Indonesia Security Lite"
+echo "And All Member Ciamis Black Hat's"
 echo "\033[30;1m tunggu 8 detik"
 sleep 8
-sh LITESPAM.sh
+sh Mrs.Major.sh
 fi
 
 if
